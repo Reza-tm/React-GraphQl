ebe6b30d6f7807b7effaa9e2601d0476ba50bb22
Reza-tm <rezatm331@gmail.com>
2022-01-01 23:11:53 +0330

    add NProgress bar

--- frontend/components/Page.js
+++ frontend/components/Page.js
@@ -1,7 +1,31 @@
 import React from "react";
 import propTypes from "prop-types";
 import Header from "../components/Header";
-import styled from "styled-components";
+import styled, { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+@font-face {
+  font-family: radnika ;
+  src: url(/static/radnikanext-medium-webfont.woff2) format('woff2'); font-weight: normal; font-style: normal;
+}
+html{
+  --red :#ff0000;
+  --black:#393939;
+  --gray:#3a3a3a;
+  --lightGray:#e1e1e1;
+  --offWhite:#ededed;
+  --bs : 0 12px 24px 0 rgba(0 , 0 ,0 ,0.09) ;
+  box-sizing: border-box;
+}
+* , *::after , *::before {
+  box-sizing: inherit;
+}
+a{
+  text-decoration: none;
+  color: var(--black);
+}
+body{font-family:'radnika' ; padding: 0;margin: 0;font-size: 1.5rem;line-height: 2;}
+`;
 
 const Wrapper = styled.div`
   max-width: 1000px;
@@ -9,6 +33,7 @@ const Wrapper = styled.div`
 
 const Page = ({ children }) => (
   <div>
+    <GlobalStyles />
     <Header />
     <Wrapper>{children}</Wrapper>
   </div>
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,30 +1,15 @@
 import React from "react";
-import styled, { createGlobalStyle } from "styled-components";
 import Page from "../components/Page";
+import NProgress from "nprogress";
+import "../components/styles/nprogress.css";
+import { Router } from "next/router";
 
-const GlobalStyles = createGlobalStyle`
-@font-face {
-  font-family: radnika ;
-  src: url(/static/radnikanext-medium-webfont.woff2) format('woff2'); font-weight: normal; font-style: normal;
-}
-html{
-  --red :#ff0000;
-  --black:#393939;
-  --gray:#3a3a3a;
-  --lightGray:#e1e1e1;
-  --offWhite:#ededed;
-  --bs : 0 12px 24px 0 rgba(0 , 0 ,0 ,0.09) ;
-  box-sizing: border-box;
-}
-* , *::after , *::before {
-  box-sizing: inherit;
-}
-body{font-family:'radnika' ; padding: 0;margin: 0;font-size: 1.5rem;line-height: 2;}
-`;
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 const MainApp = ({ Component, pageProps }) => (
   <Page>
-    <GlobalStyles />
     <Component {...pageProps} />
   </Page>
 );
