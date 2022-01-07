0649d249d2648063d4f667e00cb36c7d36581f0c
Reza-tm <rezatm331@gmail.com>
2022-01-08 00:00:34 +0330

    add apollo client config

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,15 +3,19 @@ import Page from "../components/Page";
 import NProgress from "nprogress";
 import "../components/styles/nprogress.css";
 import { Router } from "next/router";
+import withData from "../lib/withData";
+import { ApolloProvider } from "@apollo/client";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
 Router.events.on("routeChangeError", () => NProgress.done());
 
-const MainApp = ({ Component, pageProps }) => (
-  <Page>
-    <Component {...pageProps} />
-  </Page>
+const MainApp = ({ Component, pageProps, apollo }) => (
+  <ApolloProvider client={apollo}>
+    <Page>
+      <Component {...pageProps} />
+    </Page>
+  </ApolloProvider>
 );
 
-export default MainApp;
+export default withData(MainApp);
--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,20 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
 class MyDocument extends Document {
-    static async getInitialProps(ctx) {
-      const initialProps = await Document.getInitialProps(ctx)
-      return { ...initialProps }
-    }
-  
-    render() {
-      return (
-        <Html lang='en-CA'>
-          <Head />
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      )
-    }
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
-  
-  export default MyDocument
+
+  render() {
+    return (
+      <Html lang="en-CA">
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
